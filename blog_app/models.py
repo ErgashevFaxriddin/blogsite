54adc5fcdb8ef76fde8f3f05443e64692d24db08
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-24 23:09:11 +0500

    updated:

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,6 +4,22 @@ from django.utils import timezone
 from django.urls import reverse
 
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
