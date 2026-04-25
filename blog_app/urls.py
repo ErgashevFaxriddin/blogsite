d5546c8774f3fd9615285c47e6d1d0a51231f9ba
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-25 22:09:25 +0500

    urls updated

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
-from django.urls import path
 from django.conf.urls.static import static
+from django.urls import path
 from .views import PostListView, post_detail
-
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:slug>',
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          post_detail,
-         name='post_detail')
+         name='post_detail'),
 ]
 
 if settings.DEBUG:
