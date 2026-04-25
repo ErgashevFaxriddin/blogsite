7af1dcfdaf787aa6dd9ca591d7197b8ebb9594c9
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-25 22:11:47 +0500

    models updated

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,7 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import User #user uchun models tayyor, userga xususiyatlar qoshamiz
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date="publish")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
 
 
 class Comment(models.Model):
@@ -13,44 +51,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.name} — {self.post}'
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
-
-
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published')
-    )
-
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique_for_date="publish") #sana boyicha unique qiladi
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-
-    class Meta:
-        ordering = ('-publish',) # eng so'ngi xabarlar oxirida ko'rinishi uchun
-
-    def __str__(self):
-        return self.title
-
-    objects = models.Manager()
-    published = PublishedManager()
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[
-            self.publish.year,
-            self.publish.month,
-            self.publish.day,
-            self.slug
-        ])
-
-
-# posts = Post.objects.filter(status='published')
-# p_posts = Post.published.all()
