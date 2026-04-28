6cc59c26b3f5cc112e7ae32b14be3a8ce7bb1489
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-28 21:10:44 +0500

    forms updated

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,7 +1,20 @@
+# from django import forms
+# from .models import Comment
+#
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ['name', 'body']
+
+
 from django import forms
 from .models import Comment
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Sizning fikringiz...'}),
+        }
