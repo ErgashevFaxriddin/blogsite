bce5f2fa05f4e6489358396876dd182fb0c5c0a9
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-28 21:11:45 +0500

    urls updated

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,16 +1,28 @@
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.urls import path
+# from .views import PostListView, post_detail
+#
+# app_name = 'blog'
+#
+# urlpatterns = [
+#     path('', PostListView.as_view(), name='post_list'),
+#     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+#          post_detail,
+#          name='post_detail'),
+# ]
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 from django.urls import path
-from .views import PostListView, post_detail
+from .views import PostListView, PostDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
-         post_detail,
-         name='post_detail'),
+         PostDetailView.as_view(), name='post_detail'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
