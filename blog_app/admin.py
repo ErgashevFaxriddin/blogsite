1049550c467c9e21fb8e04a2325bab7640829bff
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-28 21:12:23 +0500

    admin updated

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,5 +1,20 @@
+# from django.contrib import admin
+# from .models import Post
+#
+#
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'author', 'publish', 'status')
+#     list_filter = ('status', 'created', 'publish', 'author')
+#     search_fields = ('title', 'body')
+#     prepopulated_fields = {'slug': ('title', )}
+#     raw_id_fields = ('author', )
+#     date_hierarchy = 'publish'
+#     ordering = ('status', 'publish')
+
+
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,7 +22,15 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title', )}
-    raw_id_fields = ('author', )
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
     date_hierarchy = 'publish'
-    ordering = ('status', 'publish')
+    ordering = ('-publish', 'status')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'post')
+    search_fields = ('name', 'body')
+    ordering = ('-created',)
